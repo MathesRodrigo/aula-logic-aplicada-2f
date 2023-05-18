ce9b8e4316b77453603a51e82711ad21566f9e6d
MathesRodrigo <emailgit>
2023-05-18 08:27:06 -0300

    adao e eva são power rangers

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        
+          // aula de Orientação á Objetos
+          // Declarar objetos
+        Pessoa adao;
+        // instanciar objetos
+        adao = new Pessoa();
+        Pessoa eva = new Pessoa();
+        // Definição do formato
+        adao.nome = "Adão";
+        adao.sobrenome = "silvia";
+        eva.nome = "Eva";
+        eva.sobrenome = "silvia";
+        // Acionar comportamento
+        adao.falar();
+        eva.falar();
+
     }
 }
