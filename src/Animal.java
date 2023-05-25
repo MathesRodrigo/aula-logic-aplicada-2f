09789b3eb1a7f58599ed654dc72c0a0e74524718
MathesRodrigo <emailgit>
2023-05-25 08:29:45 -0300

    Adicionado Animal

---
+++ src/Animal.java
@@ -0,0 +1,14 @@
+public class Animal {
+
+    String especie;
+    String nome;
+    int idade;
+    String cor;
+    String tipoPelo;
+    String corOlhoDiretio;
+
+    public void comer(){
+        System.out.println("animal comer");
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -26,6 +26,7 @@ public class Main {
         rainha.sobrenome = "Tunner";
         //Definiçao de comportamento
        System.out.println(rainha.falar("alto"));
+       rainha.comer();
 
     }
 }
